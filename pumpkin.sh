f975cc88c5a557bff0a01ded558fd81e23c281e0
Vinicius Prado da Fonseca <vncprado@gmail.com>
2016-11-28 18:10:25 -0500

    Script change to run only pumpkin_kinect commands

--- pumpkin.sh
+++ pumpkin.sh
@@ -3,5 +3,6 @@ MASTER=$HOSTNAME
 cd ~/workspace/pumpkin_ws/
 source devel/setup.bash
 export ROS_MASTER_URI=http://$MASTER:11311
-roslaunch pumpkin pumpkin.launch
+#roslaunch pumpkin pumpkin.launch
 roslaunch pumpkin pumpkin_kinect.launch
+
