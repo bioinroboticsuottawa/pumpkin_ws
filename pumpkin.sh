7f74d88c36d1f946b8c9a0211b2b63fcd95ce5ac
Vinicius Prado da Fonseca <vncprado@gmail.com>
2016-12-05 20:33:05 -0500

    Tests for boot. pumpkin.launch still with errors probably because ssc port.

--- pumpkin.sh
+++ pumpkin.sh
@@ -3,6 +3,12 @@ MASTER=$HOSTNAME
 cd ~/workspace/pumpkin_ws/
 source devel/setup.bash
 export ROS_MASTER_URI=http://$MASTER:11311
-#roslaunch pumpkin pumpkin.launch
-roslaunch pumpkin pumpkin_kinect.launch
-rosrun pumpkin head_init.py
+
+echo pumpkin kinect init
+roslaunch pumpkin pumpkin_kinect.launch &
+sleep 10s
+echo Head init...
+rosrun pumpkin head_init.py &
+sleep 10s
+echo pumpkin init
+#roslaunch pumpkin pumpkin.launch &
